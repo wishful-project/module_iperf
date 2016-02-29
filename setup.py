193bae32db72853aec838b84f10de5f31321c6b8
Piotr Gawlowicz <gawlowicz@tu-berlin.de>
2016-02-29 23:42:29 +0100

    update setup

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ def readme():
         return f.read()
 
 setup(
-    name='wishful_driver_iperf',
+    name='wishful_module_iperf',
     version='0.1.0',
     packages=find_packages(),
     url='http://www.wishful-project.eu/software',
     license='',
     author='Piotr Gawlowicz',
     author_email='gawlowicz@tu-berlin.de',
-    description='WiSHFUL iperf Driver',
-    long_description='Implementation of a wireless agent using the unified programming interfaces (UPIs) of the Wishful project.',
+    description='WiSHFUL Module iperf',
+    long_description='WiSHFUL Module iperf',
     keywords='wireless control',
     install_requires=[]
 )
