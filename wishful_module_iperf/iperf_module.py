3db29eb16fbf500841fe379c6d3524c656aa75c2
Piotr Gawlowicz <gawlowicz@tu-berlin.de>
2016-03-10 10:50:35 +0100

    agent module location change

--- wishful_module_iperf/iperf_module.py
+++ wishful_module_iperf/iperf_module.py
@@ -1,6 +1,6 @@
 import logging
 import wishful_upis as upis
-import wishful_agent as wishful_module
+import wishful_framework as wishful_module
 
 __author__ = "Piotr Gawlowicz, Mikolaj Chwalisz"
 __copyright__ = "Copyright (c) 2015, Technische Universität Berlin"
