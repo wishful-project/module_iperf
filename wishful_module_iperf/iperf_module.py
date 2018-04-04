4f10d1b5ad209db2f5d7a24731a96a1316d32574
pruckebusch <peter.ruckebusch@ugent.be>
2018-04-04 12:28:05 +0200

    testing iperf in thread with response send_upstream

--- wishful_module_iperf/iperf_module.py
+++ wishful_module_iperf/iperf_module.py
@@ -13,11 +13,15 @@ __email__ = "{gawlowicz, chwalisz}@tkn.tu-berlin.de"
 '''
     Packet flows are generated using IPerf tool.
 '''
+
+
 @wishful_module.build_module
 class IperfModule(wishful_module.AgentModule):
     def __init__(self):
         super(IperfModule, self).__init__()
         self.log = logging.getLogger('IperfModule.main')
+        self.packetflow_sink_active = False
+        self.packetflow_client_active = False
 
     def worker(self, port):
         """thread worker function"""
@@ -25,53 +29,66 @@ class IperfModule(wishful_module.AgentModule):
         server = iperf3.Server()
         server.port = port
         server.verbose = False
-        while True:
+        while self.packetflow_sink_active:
             server.run()
 
     @wishful_module.bind_function(upis.net.create_packetflow_sink)
     def start_server(self, port):
         self.log.debug("Starts iperf server on port {}".format(port))
-        _thread.start_new_thread(self.worker,(port))
+        self.packetflow_sink_active = True
+        _thread.start_new_thread(self.worker, (port))
         self.log.debug("Server thread is started")
-
         return "Server_started"
 
     @wishful_module.bind_function(upis.net.destroy_packetflow_sink)
     def stop_server(self):
         self.log.debug("Stops iperf server.")
-
+        self.packetflow_sink_active = False
         return "Server stops automatically after one client test"
 
-    @wishful_module.bind_function(upis.net.start_packetflow)
-    def start_packetflow(self, dest_ip, port):
-        self.log.debug("Start iperf client.")
+    def client_worker(self, dest_ip, port):
         client = iperf3.Client()
         client.duration = 10
         client.server_hostname = dest_ip
         client.port = port
         client.protocol = 'tcp'
         self.log.debug('Connecting to {0}:{1}'.format(client.server_hostname, client.port))
-        result = client.run()
-
-        if result.error:
-            return result.error
-        else:
+        while self.packetflow_client_active:
+            result = client.run()
+            cmdDesc = wishful_module.CmdDesc()
+            cmdDesc.type = "net"
+            cmdDesc.func_name = "start_packetflow"
+            cmdDesc.interface = self.interface
+            cmdDesc.serialization_type = wishful_module.CmdDesc.PICKLE
+            cmdDesc.repeat_number = 0
             output = ""
-            output+="Test completed: \n started at: "
-            output+= str(result.time)
-            output+="\n bytes transmitted: "
-            output+= str(result.bytes)
-            output+="\n jitter (ms): "
-            output+= str(result.jitter_ms)
-            output+="\n avg cpu load: "
-            output+= str(result.local_cpu_total)
-            output+="\nMegabits per second  (Mbps): "
-            output+= str(result.Mbps)
-            return output
+            if result.error:
+                self.log.debug('Connection failed {0}:{1}'.format(client.server_hostname, client.port))
+                output = result.error
+            else:
+                output = ""
+                output += "Test completed: \n started at: "
+                output += str(result.time)
+                output += "\n bytes transmitted: "
+                output += str(result.bytes)
+                output += "\n jitter (ms): "
+                output += str(result.jitter_ms)
+                output += "\n avg cpu load: "
+                output += str(result.local_cpu_total)
+                output += "\nMegabits per second  (Mbps): "
+                output += str(result.Mbps)
+            self.agent.send_upstream(["controller", cmdDesc, output])
+
+    @wishful_module.bind_function(upis.net.start_packetflow)
+    def start_packetflow(self, dest_ip, port):
+        self.log.debug("Start iperf client.")
+        self.packetflow_client_active = True
+        _thread.start_new_thread(self.worker, (port, dest_ip))
+        self.log.debug("Started iperf client.")
+        return "Client_started"
 
     @wishful_module.bind_function(upis.net.stop_packetflow)
     def stop_packetflow(self):
         self.log.debug("Stops iperf client.")
-
+        self.packetflow_client_active = False
         return "Client stopped"
-
