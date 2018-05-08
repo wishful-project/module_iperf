a9e06a33069603422933172286e6744388689f0a
Piotr Gawlowicz <gawlowicz@tu-berlin.de>
2018-05-08 16:48:48 +0200

    update Signed-off-by: Piotr Gawlowicz <gawlowicz@tu-berlin.de>

--- wishful_module_iperf/iperf_module.py
+++ wishful_module_iperf/iperf_module.py
@@ -1,7 +1,5 @@
 import logging
-import iperf3
-import gevent
-import _thread
+import subprocess
 import wishful_upis as upis
 import wishful_framework as wishful_module
 
@@ -20,78 +18,72 @@ class IperfModule(wishful_module.AgentModule):
     def __init__(self):
         super(IperfModule, self).__init__()
         self.log = logging.getLogger('IperfModule.main')
-        self.packetflow_sink_active = False
-        self.packetflow_client_active = False
-
-    def worker(self, port):
-        """thread worker function"""
-        self.log.debug("Thread is started")
-        server = iperf3.Server()
-        server.port = port
-        server.verbose = False
-        #while self.packetflow_sink_active:
-        server.run()
+        self.trafficType = "udp"
+        self.serverProc = None
+        self.clientProc = None
 
     @wishful_module.bind_function(upis.net.create_packetflow_sink)
     def start_server(self, port):
-        self.log.debug("Starts iperf server on port {}".format(port))
-        self.packetflow_sink_active = True
-        _thread.start_new_thread(self.worker, (port,))
-        self.log.debug("Server thread is started")
-        return "Server_started"
+        self.log.info("Starts iperf server on port {}".format(port))
+
+        # iperf -s -u
+        cmd = ['/usr/bin/iperf']
+        cmd.extend(['-s'])
+        cmd.extend(['-p', str(port)])
+        if self.trafficType == "udp":
+            cmd.extend(['-u'])
+
+        self.serverProc = subprocess.Popen(cmd)
+        self.log.info("iperf server started on port {} with PID: {}".format(port, self.serverProc.pid))
+        return 0
 
     @wishful_module.bind_function(upis.net.destroy_packetflow_sink)
     def stop_server(self):
-        self.log.debug("Stops iperf server.")
-        self.packetflow_sink_active = False
-        return "Server stops automatically after one client test"
+        if self.serverProc is None:
+            return
+        self.log.info("Stop iperf server with PID: {}".format(self.serverProc.pid))
 
-    def client_worker(self, dest_ip, port):
-        client = iperf3.Client()
-        client.duration = 10
-        client.server_hostname = dest_ip
-        client.port = port
-        client.protocol = 'tcp'
-        self.log.debug('Connecting to {0}:{1}'.format(client.server_hostname, client.port))
-        # while self.packetflow_client_active:
-        result = client.run()
-        cmdDesc = wishful_module.CmdDesc()
-        cmdDesc.type = "net"
-        cmdDesc.func_name = "start_packetflow"
-        #cmdDesc.interface = self.interface
-        cmdDesc.serialization_type = wishful_module.CmdDesc.PICKLE
-        cmdDesc.repeat_number = 0
-        output = ""
-        if result.error:
-            self.log.debug('Connection failed {0}:{1}'.format(client.server_hostname, client.port))
-            output = result.error
-        else:
-            output = ""
-            output += "=====Test SUCCESS: "
-            #output += "\n MBit\s sent: "
-            output += str(result.sent_Mbps)
-            #output += "\n MBit\s recv: "
-            output += " , "
-            output += str(result.received_Mbps)
-            output += "=====\n"
-                # output += "\n jitter (ms): "
-                # output += str(result.jitter_ms)
-                # output += "\n avg cpu load: "
-                # output += str(result.local_cpu_total)
-                # output += "\nMegabits per second  (Mbps): "
-                # output += str(result.Mbps)
-        self.agent.send_upstream(["controller", cmdDesc, output])
+        cmd = ['/bin/kill']
+        cmd.extend(['-9'])
+        cmd.extend([str(self.serverProc.pid)])
+        subprocess.Popen(cmd)
+        self.serverProc = None
+        self.log.info("iperf server stopped")
+
+        return 0
 
     @wishful_module.bind_function(upis.net.start_packetflow)
     def start_packetflow(self, dest_ip, port):
-        self.log.debug("Start iperf client.")
-        self.packetflow_client_active = True
-        _thread.start_new_thread(self.client_worker, (dest_ip, port))
-        self.log.debug("Started iperf client.")
-        return "Client_started"
+        self.log.info("Start iperf client to server IP: {}".format(dest_ip))
+        # iperf -c 100.0.0.19 -t 1000000000 -u -b 200
+
+        serverIp = dest_ip
+        remotePort = port
+        transmissionTime = 100000000
+        bandwidth = '200M'
+
+        cmd = ['/usr/bin/iperf']
+        cmd.extend(['-c', str(serverIp)])
+        cmd.extend(['-t', str(transmissionTime)])
+        cmd.extend(['-p', str(remotePort)])
+        if self.trafficType == "udp":
+            cmd.extend(['-u'])
+            cmd.extend(['-b', str(bandwidth)])
+
+        self.clientProc = subprocess.Popen(cmd)
+        self.log.info("iperf client started on port {} with PID: {}".format(port, self.clientProc.pid))
+        return 0
 
     @wishful_module.bind_function(upis.net.stop_packetflow)
     def stop_packetflow(self):
-        self.log.debug("Stops iperf client.")
-        self.packetflow_client_active = False
-        return "Client stopped"
+        if self.clientProc is None:
+            return
+        self.log.info("Stop iperf client with PID: {}".format(self.clientProc.pid))
+
+        cmd = ['/bin/kill']
+        cmd.extend(['-9'])
+        cmd.extend([str(self.clientProc.pid)])
+        subprocess.Popen(cmd)
+        self.clientProc = None
+        self.log.info("iperf client stopped")
+        return 0
